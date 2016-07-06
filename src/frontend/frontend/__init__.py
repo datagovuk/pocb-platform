c3b8e182fe92fbbcf4e744f64f1f938cbf90b1f6
Ross Jones <ross@servercode.co.uk>
2016-07-06 13:47:12 +0100

    Error templates

--- src/frontend/frontend/__init__.py
+++ src/frontend/frontend/__init__.py
@@ -10,3 +10,15 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template('500.html'), 500
+
+@app.errorhandler(401)
+def permission_denied(e):
+    return render_template('401.html'), 401
--- src/pubtool/pubtool/__init__.py
+++ src/pubtool/pubtool/__init__.py
@@ -11,3 +11,15 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 def home():
     print( current_app.config["ELASTIC_HOSTS"])
     return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template('500.html'), 500
+
+@app.errorhandler(401)
+def permission_denied(e):
+    return render_template('401.html'), 401
