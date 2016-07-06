046587f099101de5c43f3063137dc8cbfa4338f8
Ross Jones <ross@servercode.co.uk>
2016-07-06 13:25:52 +0100

    Add templates and static content for frontend

--- src/frontend/frontend/__init__.py
+++ src/frontend/frontend/__init__.py
@@ -8,5 +8,5 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 #create_filters(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World from frontend!'
+def home():
+    return render_template("index.html")
