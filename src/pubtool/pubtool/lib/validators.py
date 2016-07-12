3b2fef2f5987c473c90084b78f465ddd6a454005
Ross Jones <ross@servercode.co.uk>
2016-07-12 14:29:10 +0100

    Explain the validator functions

--- src/pubtool/pubtool/lib/validators.py
+++ src/pubtool/pubtool/lib/validators.py
@@ -1,3 +1,14 @@
+'''
+A collection of validation functions.  These are registered in pubtool.lib.validators with a
+name that appears in the JSON Schema.
+
+The functions here whould contain:
+    validator - The validator that is being used
+    value - The value of the validation function, i.e. the name it was registered with
+    instance - The value of the property
+    schema - The schema subset for the current property
+'''
+
 from pubtool.database import mongo
 
 from jsonschema.exceptions import ValidationError
